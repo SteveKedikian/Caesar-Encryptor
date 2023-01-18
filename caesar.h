9db014d56dbb4920bbe551b7bc691a047323ce36
Steve Kedikian <105996903+SteveKedikian@users.noreply.github.com>
2023-01-18 23:55:50 +0400

    Update caesar.h

--- caesar.h
+++ caesar.h
@@ -1,4 +1,17 @@
+#ifndef CAESAR_H
+#define CAESAR_H
+
 #include <string>
+
+enum {
+    LETTER_COUNT = 26,
+    CAPITAL_A = 65,
+    CAPITAL_Z = 90,
+    SMALL_a = 97,
+    SMALL_z = 122
+};
 
 std::string caesar_encrypt(const std::string& line, const int key);
 std::string caesar_decoder(const std::string& line, const int key);
+
+#endif // CAESAR_H
